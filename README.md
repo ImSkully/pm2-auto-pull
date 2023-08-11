03420625bb2366642d264a84b7d41c61460b5c81
Skully <contact@skully.tech>
2023-08-11 01:58:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# PM2 Auto Pull <a href="https://pm2.keymetrics.io" target="_blank" alt="pm2"><img src="https://img.shields.io/badge/pm2-2B037A.svg?logo=pm2" /></a>
+
+![Maintained](https://img.shields.io/maintenance/yes/2023.svg)
+<a href="https://nodejs.org" target="_blank" alt="Node.js"><img src="https://img.shields.io/badge/Node.js-6DA55F?style=flat&logo=node.js&logoColor=white" /></a>
+![License](https://img.shields.io/github/license/ImSkully/pm2-auto-pull)
+<a href="./package-lock.json" target="_blank" alt="package-lock"><img src="https://img.shields.io/badge/package--lock-committed-brightgreen" /></a>
+<a href="https://www.npmjs.com/package/@imskully\/pm2-auto-pull" target="_blank" alt="npmjs Package"><img src="https://img.shields.io/npm/v/@imskully\/pm2-auto-pull.svg" /></a>
+
+A simple and efficient [PM2](https://pm2.keymetrics.io) module that automatically pulls the latest git version of your application every set interval.
+
+* ⚡️ Fast and lightweight with minimal overhead!
+* 🔧 Easy to install, ready to go immediately after installation
+* 📝 Adjustable update-check interval to pull at specific intervals
+* 🖱️ PM2 Dashboard action to manually trigger update fetches
+
+# Installation & Setup
+1. Install the module via `pm2`:
+	```bash
+	pm2 install @imskully/pm2-auto-pull
+	```
+	> [!IMPORTANT]  
+	> Use `pm2 install`, not `npm install`!
+
+2. *(Optional)* The default fetch interval is 30 seconds, configure the update interval with the following command:
+	```bash
+	pm2 set @imskully@pm2-auto-pull:interval <ms>
+	```
+
+3. The module should restart and begin fetching on the configured interval!
+
+# Usage
+
+Once the module is installed and configured, it will automatically begin checking for any updates for all running processes from their respective connected git version controls.
+
+> [!WARNING]  
+> This module will `git reset --hard` to the latest version (`HEAD`) from your configured git remote and will overwrite any local changes that have not been committed or stashed!
+
+You can optionally enable verbose logging to see when an interval check is run along with an output of all processes that were checked, skipped, and updated:
+```bash
+pm2 set @imskully/pm2-auto-pull:logging true
+```
+
+At any time, you can view when the last update check has run and other configured settings with:
+```bash
+pm2 show @imskully/pm2-auto-pull
+```
+
+# Uninstalling
+To uninstall the module, simply run:
+```bash
+pm2 uninstall @imskully/pm2-auto-pull
+```
+
+This will remove the module from your PM2 configuration and stop it from running, you may also want to remove any configuration variables that were set:
+```bash
+pm2 unset @imskully/pm2-auto-pull:interval
+pm2 unset @imskully/pm2-auto-pull:logging
+```
---
+++ app.js
@@ -0,0 +1,104 @@
+const io = require ("@pm2/io");
+const pm2 = require("pm2");
+const async = require("async");
+
+let IS_FETCHING = false; // Whether we are currently fetching the latest version for all processes.
+let LAST_CHECK = false; // The last time we checked for updates.
+
+/**
+ * fetchLatestVersion()
+ * Fetches the latest git version for all connected pm2 processes.
+ */
+async function fetchLatestVersion()
+{
+	if (IS_FETCHING) return; // Already fetching, skip.
+
+	log("Fetching latest version for all processes..");
+	LAST_CHECK = Date.now();
+
+	return new Promise(async (resolve, reject) => {
+		// Fetch all processes.
+		pm2.list((error, allProcesses) => {
+			if (error) {
+				console.error("[pm2-auto-pull]: Error whilst fetching process list!", error);
+				IS_FETCHING = false;
+				return reject(error);
+			}
+
+			// Keep track of each process we check and their outcome.
+			let fetchStats = { checked: [], updated: [], skipped: [] }
+
+			// Iterate through all processes and pull latest version.
+			async.forEachLimit(allProcesses, 1, (process, next) => {
+				fetchStats.checked.push(process.name);
+
+				if (
+					!process?.pm2_env || // Skip if no pm2_env available.
+					process?.pm2_env != "online" || // Or if the process is not online.
+					!process.pm2_env?.versioning // Or if the process has no configured version control.
+				) {
+					fetchStats.skipped.push(process.name);
+					return next();
+				}
+
+				log(`Checking updates for ${process.name}..`);
+				pm2.pullAndReload(process.name, (error, metadata) => {
+					if (error) return next(error); // Already up to date.
+
+					if (metadata) log(`Successfully pulled latest version for '${process.name}'!`, true);
+					fetchStats.updated.push(process.name);
+					return next();
+				});
+			}, () => {
+				// All processes have been checked.
+				IS_FETCHING = false;
+				log(fetchStats);
+				return resolve(fetchStats);
+			});
+		});
+	});
+}
+
+/**
+ * log(message, [force = false])
+ * Logs a message to the console as this module.
+ * 
+ * @param {String} 	message 	The message to log.
+ * @param {Boolean} force 		Whether to force log the message regardless of logging setting.
+ */
+function log(message, force = false)
+{
+	if (!force && !io.getConfig()?.logging) return;
+	return console.log("[pm2-auto-pull]:", message);
+}
+
+// pm2 module configuration and initialization.
+io.init({
+	human_info: [
+		["Update Check Interval", `${io.getConfig()?.interval || 30000}ms`],
+		["Last Check", (LAST_CHECK ? new Date(LAST_CHECK).toLocaleString() : "Never")],
+		["Verbose Logging", io.getConfig()?.logging ? "Enabled" : "Disabled"]
+	]
+}).initModule({}, (error) => {
+	if (error) return console.error("[pm2-auto-pull]: Failed to initialize module!", error);
+
+	// Parse interval value.
+	let FETCH_INTERVAL = parseInt(io.getConfig()?.interval); // How often to check for updates (in ms)
+	if (!FETCH_INTERVAL || FETCH_INTERVAL < 1000) FETCH_INTERVAL = 30000; // Fallback to default if invalid value provided.
+
+	pm2.connect(() => {
+		setInterval(fetchLatestVersion, FETCH_INTERVAL); // Start fetching latest version every interval.
+		log(`Connected to pm2 instance and now updating git every ${FETCH_INTERVAL}ms!`, true);
+	});
+});
+
+// Expose pm2 action to manually trigger update check.
+io.action("fetch updates", async (callback) => {
+	console.log("[Fetch Updates] Manually fetching updates..");
+	const fetchStats = await fetchLatestVersion().catch(() => { return callback({ status: "error" }); });
+
+	// Prepare callback message, we only want to include stats in the response if logging is disabled to avoid duplicate output.
+	let callbackMessage = { status: "success" }
+	if (!io.getConfig()?.logging) callbackMessage = { ...callbackMessage, ...fetchStats };
+	return callback(callbackMessage);
+});
